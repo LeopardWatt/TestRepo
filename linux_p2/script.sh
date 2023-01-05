dc825bb074c864b65dd0370cbad5e2242d54ab1b
LeopardWatt <leopard.watt@gmail.com>
2023-01-05 18:01:34 +0200

    ok_v3

--- linux_p2/script.sh
+++ linux_p2/script.sh
@@ -3,23 +3,22 @@ directory=$(pwd)
 backup_dir=$directory/backup/
 old_backup_dir=$directory/old_backup/
 log_dir=$directory/log/
-date_1=`date -I`
+date_time=`date +%F_%T`
 list=`ls $directory | grep .txt`
 log=$log_dir/backup.log
 err=$log_dir/err_backup.log
 
-#move files from directory older than 5 min
+#create directories
 mkdir -p $backup_dir $old_backup_dir $log_dir
+
+#move files from directory older than 5 min
 find $backup_dir -type f -mmin +5 -name "*tar.gz" -exec mv {} $old_backup_dir \; 2>>$err 1>>$log
 
 #create new one
 for file in $list
 do
-tar -zcvf $backup_dir$file"_"$date_1.tar.gz $file 1>>$log 2>>$err
+tar -zcvf $backup_dir$file"_"$date_time.tar.gz $file 1>>$log 2>>$err
 done
 
 # delete files older than 30min
-for file in $list
-do
-find . -name "*.txt" -type f -mmin +1 -delete
-done
+cd $old_backup_dir && find . -name "*.tar.gz" -type f -mmin +30 -delete
